76578c8dc58e2f5f93f9f0316eafa49c6ee554ff
Norgius <dmitry.schadrin21@yandex.ru>
2023-03-13 15:06:19 +0300

    initial commit

---
+++ bot.py
@@ -0,0 +1,305 @@
+import logging
+from textwrap import dedent
+
+import redis
+import requests
+from environs import Env
+from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
+from telegram.ext import Filters, Updater, CallbackContext
+from telegram.ext import CallbackQueryHandler, CommandHandler, MessageHandler
+
+from utils import (get_access_token, get_products, get_product_image,
+                   put_product_in_cart, get_user_cart, create_customer,
+                   delete_cart_product, delete_all_cart_products)
+
+logger = logging.getLogger(__name__)
+_database = None
+
+
+def parse_products(raw_products: list, inventories: list) -> dict:
+    products = {}
+    for raw_product, inventory in zip(raw_products, inventories):
+        attributes = raw_product.get('attributes')
+        product = {
+            'name': attributes.get('name'),
+            'description': attributes.get('description'),
+            'price': attributes.get('price').get('USD').get('amount') / 100,
+            'stock': inventory.get('available'),
+            'image_id': raw_product.get('relationships')
+                                   .get('main_image').get('data').get('id')
+            }
+        products[raw_product.get('id')] = product
+    return products
+
+
+def get_menu_button(products: dict) -> list:
+    keyboard = []
+    for product_id, product in products.items():
+        button = [
+            InlineKeyboardButton(product.get('name'), callback_data=product_id)
+                  ]
+        keyboard.append(button)
+    keyboard.append([InlineKeyboardButton('Корзина', callback_data='Корзина')])
+    return keyboard
+
+
+def start(update: Update, context: CallbackContext) -> str:
+    products = context.bot_data['products']
+    keyboard = get_menu_button(products)
+    reply_markup = InlineKeyboardMarkup(keyboard)
+    update.message.reply_text(text='Пожалуйста, выберите товар!',
+                              reply_markup=reply_markup)
+    return 'HANDLE_MENU'
+
+
+def handle_menu(update: Update, context: CallbackContext) -> str:
+    bot = context.bot
+    query = update.callback_query
+    chat_id = query.message.chat_id
+    if query.data == 'Назад':
+        bot.delete_message(chat_id=query.message.chat_id,
+                           message_id=query.message.message_id)
+        return 'MENU'
+    product_id = context.bot_data['product_id']
+    product_data = context.bot_data['products'].get(product_id)
+    image_id = product_data.get('image_id')
+    store_access_token = context.bot_data['store_access_token']
+
+    image = get_product_image(store_access_token, image_id)
+    message = dedent(f'''
+    {product_data.get('name')}
+
+    {product_data.get('price'):.2f}$ за 1 кг
+    {product_data.get('stock')}кг на складе
+
+    {product_data.get('description')}
+    ''')
+    keyboard = [
+        [InlineKeyboardButton('1 кг', callback_data='1 кг'),
+         InlineKeyboardButton('5 кг', callback_data='5 кг'),
+         InlineKeyboardButton('10 кг', callback_data='10 кг')],
+        [InlineKeyboardButton('Корзина', callback_data='Корзина')],
+        [InlineKeyboardButton('Назад', callback_data='Назад')]
+    ]
+    reply_markup = InlineKeyboardMarkup(keyboard)
+    bot.delete_message(chat_id=chat_id,
+                       message_id=query.message.message_id)
+    bot.send_photo(chat_id=chat_id, photo=image, caption=message,
+                   reply_markup=reply_markup)
+    return 'HANDLE_DESCRIPTION'
+
+
+def handle_description(update: Update, context: CallbackContext) -> str:
+    bot = context.bot
+    query = update.callback_query
+    user_reply = query.data
+    chat_id = query.message.chat_id
+    if user_reply in ['1 кг', '5 кг', '10 кг']:
+        store_access_token = context.bot_data['store_access_token']
+        product_id = context.bot_data['product_id']
+        quantity = int(user_reply.split()[0])
+        put_product_in_cart(store_access_token, product_id,
+                            quantity, chat_id)
+        return 'HANDLE_DESCRIPTION'
+
+    products = context.bot_data['products']
+    keyboard = get_menu_button(products)
+    reply_markup = InlineKeyboardMarkup(keyboard)
+
+    bot.delete_message(chat_id=chat_id,
+                       message_id=query.message.message_id)
+    bot.send_message(text='Пожалуйста, выберите товар!', chat_id=chat_id,
+                     reply_markup=reply_markup)
+    return 'HANDLE_MENU'
+
+
+def handle_cart(update: Update, context: CallbackContext) -> str:
+    bot = context.bot
+    query = update.callback_query
+    chat_id = query.message.chat_id
+    user_reply = query.data
+    store_access_token = context.bot_data['store_access_token']
+    if user_reply.startswith('del_'):
+        product_id = user_reply[4::]
+        delete_cart_product(store_access_token, chat_id, product_id)
+
+    user_cart = get_user_cart(store_access_token, chat_id)
+    products = user_cart.get('data')
+    message = ''
+    keyboard = []
+    if products:
+        for product in products:
+            product_price = product.get('meta').get('display_price')\
+                .get('without_tax').get('unit').get('formatted')
+            product_quantity = product.get('quantity')
+            product_total_cost = product_quantity * float(product_price[1::])
+
+            message += dedent(f'''
+            {product.get("name")}
+            {product.get("description")}
+            {product_price} за кг
+            {product_quantity}кг в корзине за ${product_total_cost:.2f}
+            ''')
+            button = [InlineKeyboardButton(
+                f'Убрать из корзины {product.get("name")}',
+                callback_data=f'del_{product.get("id")}'
+                                           )
+                      ]
+            keyboard.append(button)
+        cart_total_cost = user_cart.get('meta').get('display_price')\
+            .get('without_tax').get('formatted')
+        message += f'\nОбщая стоимость: {cart_total_cost}'
+    else:
+        message = 'Ваша корзина пуста'
+
+    keyboard.append([InlineKeyboardButton('В меню', callback_data='В меню')])
+    if len(keyboard) > 1:
+        keyboard.append(
+            [InlineKeyboardButton('Оплатить', callback_data='Оплатить')]
+        )
+    reply_markup = InlineKeyboardMarkup(keyboard)
+    bot.delete_message(chat_id=chat_id,
+                       message_id=query.message.message_id)
+    bot.send_message(chat_id=chat_id, text=message, reply_markup=reply_markup)
+    return 'HANDLE_DESCRIPTION'
+
+
+def waiting_email(update: Update, context: CallbackContext) -> str:
+    bot = context.bot
+    query = update.callback_query
+    if query and query.data in ['Оплатить', 'Неверно']:
+        message = 'Пришлите, пожалуйста, ваш email'
+        bot.send_message(text=message, chat_id=query.message.chat_id)
+        return 'WAITING_EMAIL'
+    elif query and query.data == 'Верно':
+        store_access_token = context.bot_data['store_access_token']
+        products = context.bot_data['products']
+        chat_id = query.message.chat_id
+        bot.delete_message(chat_id=chat_id,
+                           message_id=query.message.message_id)
+        bot.send_message(text='Ожидайте уведомление на почте', chat_id=chat_id)
+
+        if not _database.get(f'customer_{chat_id}'):
+            first_name = name if (name := query.from_user.first_name) else ''
+            last_name = name if (name := query.from_user.last_name) else ''
+            customer_name = (first_name + ' ' + last_name).strip()
+            store_access_token = context.bot_data['store_access_token']
+            customer_email = _database.get(f'email_{chat_id}').decode()
+            customer_id = create_customer(store_access_token, customer_name,
+                                          customer_email)
+            _database.set(f'customer_{chat_id}', customer_id)
+        delete_all_cart_products(store_access_token, chat_id)
+        keyboard = get_menu_button(products)
+        reply_markup = InlineKeyboardMarkup(keyboard)
+        bot.send_message(text='Пожалуйста, выберите товар!', chat_id=chat_id,
+                         reply_markup=reply_markup)
+        return 'HANDLE_MENU'
+    else:
+        email = update.message.text
+        message = dedent(f'''
+        Вы прислали мне эту почту: {email}
+        Всё верно?
+        ''')
+        chat_id = update.effective_chat.id
+        _database.set(f'email_{chat_id}', email)
+        keyboard = [[InlineKeyboardButton('Верно', callback_data='Верно')],
+                    [InlineKeyboardButton('Неверно', callback_data='Неверно')]]
+        reply_markup = InlineKeyboardMarkup(keyboard)
+        update.message.reply_text(text=message, reply_markup=reply_markup)
+        return 'WAITING_EMAIL'
+
+
+def handle_users_reply(update: Update, context: CallbackContext) -> None:
+    env = Env()
+    env.read_env()
+    client_secret = env.str('ELASTICPATH_CLIENT_SECRET')
+    client_id = env.str('ELASTICPATH_CLIENT_ID')
+    try:
+        db = get_database_connection(env)
+        store_access_token = db.get('store_access_token')
+        if not store_access_token:
+            store_access_token = get_access_token(_database, client_secret,
+                                                  client_id)
+        else:
+            store_access_token = store_access_token.decode('utf-8')
+        raw_products, inventories = get_products(store_access_token)
+        products = parse_products(raw_products, inventories)
+        context.bot_data['products'] = products
+        context.bot_data['store_access_token'] = store_access_token
+    except requests.exceptions.HTTPError as err:
+        logger.warning(f'Ошибка в работе api.moltin.com\n{err}\n')
+
+    if update.message:
+        user_reply = update.message.text
+        chat_id = update.message.chat_id
+    elif update.callback_query:
+        user_reply = update.callback_query.data
+        chat_id = update.callback_query.message.chat_id
+    else:
+        return
+
+    if user_reply == '/start':
+        user_state = 'START'
+    elif user_reply in products:
+        user_state = 'HANDLE_MENU'
+        context.bot_data['product_id'] = user_reply
+    elif user_reply in ['Назад', '1 кг', '5 кг', '10 кг']:
+        user_state = 'HANDLE_DESCRIPTION'
+    elif user_reply == 'Корзина' or user_reply.startswith('del_'):
+        user_state = 'HANDLE_CART'
+    elif user_reply == 'Оплатить':
+        user_state = 'WAITING_EMAIL'
+    else:
+        user_state = db.get(chat_id).decode('utf-8')
+
+    states_functions = {
+        'START': start,
+        'HANDLE_MENU': handle_menu,
+        'HANDLE_DESCRIPTION': handle_description,
+        'HANDLE_CART': handle_cart,
+        'WAITING_EMAIL': waiting_email,
+    }
+    state_handler = states_functions[user_state]
+    try:
+        next_state = state_handler(update, context)
+        db.set(chat_id, next_state)
+    except requests.exceptions.HTTPError as err:
+        logger.warning(f'Ошибка в работе api.moltin.com\n{err}\n')
+    except Exception as err:
+        logger.warning(f'Ошибка в работе телеграм бота\n{err}\n')
+
+
+def get_database_connection(env: Env) -> redis.Redis:
+    global _database
+    if _database is None:
+        database_password = env.str("REDIS_PASSWORD")
+        database_host = env.str("REDIS_HOST")
+        database_port = env.int("REDIS_PORT")
+        _database = redis.Redis(host=database_host, port=database_port,
+                                password=database_password)
+    return _database
+
+
+def main():
+    env = Env()
+    env.read_env()
+
+    logging.basicConfig(
+        format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+        level=logging.INFO
+    )
+    logger.setLevel(logging.INFO)
+
+    tg_token = env.str('FISH_SHOP_BOT_TG_TOKEN')
+    updater = Updater(tg_token)
+    dispatcher = updater.dispatcher
+    dispatcher.add_handler(CallbackQueryHandler(handle_users_reply))
+    dispatcher.add_handler(MessageHandler(Filters.text, handle_users_reply))
+    dispatcher.add_handler(CommandHandler('start', handle_users_reply))
+    logger.info('Телеграм бот запущен')
+    updater.start_polling()
+    updater.idle()
+
+
+if __name__ == '__main__':
+    main()
---
+++ utils.py
@@ -0,0 +1,85 @@
+import requests
+import redis
+
+ONE_HOUR = 3600
+
+
+def get_access_token(_database: redis.Redis, client_secret: str,
+                     client_id: str) -> str:
+    url = 'https://api.moltin.com/oauth/access_token'
+    data = {'grant_type': 'client_credentials',
+            'client_secret': client_secret, 'client_id': client_id}
+    response = requests.post(url, data=data)
+    response.raise_for_status()
+    access_token = response.json().get('access_token')
+    _database.setex('store_access_token', ONE_HOUR, access_token)
+    return access_token
+
+
+def get_products(store_access_token: str) -> tuple[list, list]:
+    headers = {'Authorization': f'Bearer {store_access_token}'}
+    response = requests.get('https://api.moltin.com/catalog/products',
+                            headers=headers)
+    response.raise_for_status()
+    raw_products = response.json().get('data')
+    response = requests.get('https://api.moltin.com/v2/inventories',
+                            headers=headers)
+    response.raise_for_status()
+    inventories = response.json().get('data')
+    return raw_products, inventories
+
+
+def get_product_image(store_access_token: str, image_id: str):
+    headers = {'Authorization': f'Bearer {store_access_token}'}
+    response = requests.get(f'https://api.moltin.com/v2/files/{image_id}',
+                            headers=headers)
+    response.raise_for_status()
+    image_link = response.json().get('data').get('link').get('href')
+    response = requests.get(image_link, stream=True)
+    response.raise_for_status()
+    return response.raw
+
+
+def put_product_in_cart(store_access_token: str, product_id: str,
+                        quantity: str, chat_id: int) -> None:
+    url = f'https://api.moltin.com/v2/carts/{chat_id}/items'
+    headers = {'Authorization': f'Bearer {store_access_token}'}
+    body = {"data": {'quantity': quantity, 'type': 'cart_item',
+                     'id': product_id}}
+    response = requests.post(url, headers=headers, json=body)
+    response.raise_for_status()
+
+
+def get_user_cart(store_access_token: str, chat_id: int) -> dict:
+    url = f'https://api.moltin.com/v2/carts/{chat_id}/items'
+    headers = {'Authorization': f'Bearer {store_access_token}'}
+    response = requests.get(url, headers=headers)
+    response.raise_for_status()
+    return response.json()
+
+
+def delete_cart_product(store_access_token: str, chat_id: int,
+                        product_id: str) -> None:
+    url = f'https://api.moltin.com/v2/carts/{chat_id}/items/{product_id}'
+    headers = {'Authorization': f'Bearer {store_access_token}'}
+    response = requests.delete(url, headers=headers)
+    response.raise_for_status()
+
+
+def delete_all_cart_products(store_access_token: str, chat_id: int) -> None:
+    url = f'https://api.moltin.com/v2/carts/{chat_id}/items'
+    headers = {'Authorization': f'Bearer {store_access_token}'}
+    response = requests.delete(url, headers=headers)
+    response.raise_for_status()
+
+
+def create_customer(store_access_token: str, customer_name: str,
+                    customer_email: str) -> str:
+    url = 'https://api.moltin.com/v2/customers'
+    headers = {'Authorization': f'Bearer {store_access_token}'}
+    body = {"data": {'name': customer_name, 'type': 'customer',
+                     'email': customer_email}}
+    response = requests.post(url, headers=headers, json=body)
+    response.raise_for_status()
+    customer_id = response.json().get('data').get('id')
+    return customer_id
